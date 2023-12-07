50038c1f4d6b787ce44a72da761e2f02aa82e701
e.eremin <e.eremin@leningrad.io>
2023-12-07 16:16:36 +0300

    app renamed

--- android/app/src/main/java/com/lcmwkasc/MainApplication.java
+++ android/app/src/main/java/com/lcmwkasc/MainApplication.java
@@ -1,4 +1,4 @@
-package com.lcmwkasc;
+package com.OslikAI;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/LCMWKASCTests/LCMWKASCTests.m
+++ ios/LCMWKASCTests/LCMWKASCTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface LCMWKASCTests : XCTestCase
+@interface OslikAITests : XCTestCase
 
 @end
 
-@implementation LCMWKASCTests
+@implementation OslikAITests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
